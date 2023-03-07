2af04946f5540095bbe30a634bf57eb10e2fda15
Puwuky <soyiffy.ai@gmail.com>
2023-03-07 12:06:50 -0300

    Initial commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,11 @@
+import Chat from '@/components/Chat';
+
+export default function Home() {
+  return (
+      <div className="main">
+      <main className="w-full h-screen flex items-center justify-center">
+      <Chat />
+      </main>
+    </div>
+  );
+}
